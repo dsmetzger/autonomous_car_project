5bf216d029dcbbf54d06dc23d6f69a32d149b6f7
Dylan <dylan.s.metzger@gmail.com>
2015-12-29 12:50:54 -0500

    wip

--- autonomy.py
+++ autonomy.py
@@ -1,25 +1,53 @@
 import thread
+import cv2
+import numpy as np
+import time
+
 
 global coordinates=[]#holds the waypoints to navigate to, odd indexs should be end of straight section of sidewalk.
 global state="drive"#the current state of the robot.
-global stage=0#which sidewalk is the robot on.
+global stage=0#which section of sidewalk the robot is on. This will be the second phase of the project.
 
 class recognition:
 	def __init__(self):
 		#Begins camera connection
+		self.cap = cv2.VideoCapture(1)
 	def get_img(self):
 		#update current image.
+		while 1:
+			ret, frame = self.cap.read()
+			if ret==True:#check if cap.read() returned frame
+				self.img=frame
+				break
 	def get_lines(self):
 		#function that returns a series of lines in the current image. Possibly using an input region of interest of the image.
-	def white_det(self):
-		#returns pixel values that match that of the sidewalk.
+		pass
+	def wh_det(img,x1=10,x2=640,y1=470,y2=0,xit=10,yit=-10):
+		tot=0;
+		l1=[]
+		for x in range(x1,x2,xit):
+			test=3#used to ignore noise in the image. if the values goes too low the system stops incrementing in that y direction
+			for y in range(y1,y2,yit):
+				bgr=self.img[y,x]
+				if abs(int(bgr[1])-int(bgr[2]))<14 and ((bgr[0]*1.25)>(bgr[1]+bgr[2])/2):#works
+					tot+=1
+					if test<3:
+						test+=1
+					else:
+						test-=1
+					if test<2:
+						break
+			l1.append(ytot)
+		return l1
+                    
+
 class GPS:
 	def __init__(self):
 		#start the gps connection
 		#attribute of current gps position, current speed, and estimated direction vector of cumulative gps positions.
 		u = mraa.Uart(0)
 		path=u.getDevicePath()
-		self.f=open(path,'r')
+.		self.f=open(path,'r')
 	def read_data(self,data_str='$GPGGA',iterations=50):
 		#wait for optional checksum and specified identifier, return data.
 		for i in range(iterations):
@@ -60,7 +88,7 @@ class car:
 def __main__()
 	#create regocnition object, GPS object, and car instance.
 
-	#continous funcion that uses the gps vector attribute, position on sidewalk, and angle to sidewalk to create movement calculations.
+	#continous funcion that uses the gps vector attributes, position on sidewalk, and angle to sidewalk to create movement calculations.
 	while True:
 		#use gps to determine if turn is coming up
 		#possibly use eigenvectors to determine stage
@@ -68,8 +96,10 @@ def __main__()
 		#stage will be constant at 0 for now
 		if stage==0:
 			if state=='drive':
-				pass
+				#keep vehicle close to right side of sidewalk.				
 			elif state=='turn':
+				#keep moving until the edge of the camera detects white
+				#when detected, turn in a constant circle until the edge of the side walk is detected within a specified distance. orientate and drive.
 				pass
 			elif state=='ob_det':
 				pass
