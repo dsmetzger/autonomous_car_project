21f84275a6eb73fef696d6ac17850cdfa4d5f1e1
Dylan <dylan.s.metzger@gmail.com>
2016-02-24 23:03:39 -0500

    wip

---
+++ testing/GPS/get_nmea.py
@@ -0,0 +1,23 @@
+import Adafruit_BBIO.UART as UART
+import serial
+
+def parse(in_str,gps):
+	#print in_str
+	segmented_data=in_str.split(',')
+	if segmented_data[0]=='$GPGGA':# and segmented_data[6]!=0:
+		print gps
+		print in_str
+		#return lat,lon
+
+def __main__():
+	UART.setup("UART1")
+	UART.setup("UART4")
+	GPS1 = serial.Serial('/dev/ttyO1', 4800)
+	GPS4 = serial.Serial('/dev/ttyO4', 4800)
+	while(1):
+		if GPS1.inWaiting() ==1:
+			parse(GPS1.readline(),1)	 
+		#if GPS4.inWaiting() ==1:
+		#	parse(GPS4.readline(),4)
+
+__main__()
--- testing/get_direction.py
+++ testing/get_direction.py
@@ -1,13 +1,13 @@
 import smbus
 import Adafruit_BBIO.GPIO as GPIO
+from Adafruit_I2C import Adafruit_I2C
 
-
-if __name__ == "__main__":
-	GPIO.setup("P8_14", GPIO.IN)
+get_direction():
+	GPIO.setup("P8_09", GPIO.IN)
 	#read adress 0x3D
 	bus = Adafruit_I2C(address=0x3D)
 	#check if dready is high
-	if GPIO.input("P8_14"):
+	if GPIO.input("P8_09"):
 		#register address(in hex)
 		#03 msb of x
 		#04 lsb
@@ -15,9 +15,15 @@ if __name__ == "__main__":
 		#08 lsb
 		x1=bus.readList(self, 0x03, 8)
 		x2=bus.readList(self, 0x04, 8)
-
 		y1=bus.readList(self, 0x07, 8)
 		y2=bus.readList(self, 0x08, 8)
 		print x1,x2
 		print y1,y2
 		#concatanate and convert to float.
+		return 0
+	else:
+		print 'compass not ready'
+		return 1
+if __name__ == "__main__":
+	while(get_direction()):
+		pass
