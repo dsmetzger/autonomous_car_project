7e6aeb4cb817589bbdf449b8c4db77af395e6544
Dylan <dylan.s.metzger@gmail.com>
2016-02-26 18:43:54 -0500

    wip;

--- testing/get_direction.py
+++ testing/get_direction.py
@@ -2,28 +2,40 @@ import smbus
 import Adafruit_BBIO.GPIO as GPIO
 from Adafruit_I2C import Adafruit_I2C
 
-get_direction():
-	GPIO.setup("P8_09", GPIO.IN)
+def get_direction():
+	GPIO.setup("P8_10", GPIO.IN)
 	#read adress 0x3D
-	bus = Adafruit_I2C(address=0x3D)
+	bus = Adafruit_I2C(address=0x1E)
 	#check if dready is high
-	if GPIO.input("P8_09"):
+	if GPIO.input("P8_10"):
 		#register address(in hex)
 		#03 msb of x
 		#04 lsb
 		#07 msb of y
 		#08 lsb
-		x1=bus.readList(self, 0x03, 8)
-		x2=bus.readList(self, 0x04, 8)
-		y1=bus.readList(self, 0x07, 8)
-		y2=bus.readList(self, 0x08, 8)
+		print bus.readU8(9)
+		x1=bus.readU8(3)
+		x2=bus.readU8(4)
+		z1=bus.readU8(5)
+		z2=bus.readU8(6)
+		y1=bus.readU8(7)
+		y2=bus.readU8(8)
+		#to binary
+		
+		#concatenate
+		
+		#2's complement
+		
 		print x1,x2
 		print y1,y2
+		print z1,z2
 		#concatanate and convert to float.
 		return 0
+
 	else:
 		print 'compass not ready'
 		return 1
 if __name__ == "__main__":
-	while(get_direction()):
-		pass
+	print get_direction()	
+	#while(get_direction()):
+	#	pass
