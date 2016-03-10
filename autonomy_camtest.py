d55442201213f7fd5c40682ddb19f6aa0790097f
Dylan <dylan.s.metzger@gmail.com>
2016-03-10 17:35:43 -0500

    wip

--- autonomy_camtest.py
+++ autonomy_camtest.py
@@ -2,8 +2,6 @@ import thread
 import cv2
 import numpy as np
 import time
-import Adafruit_BBIO.PWM as PWM
-import Adafruit_BBIO.GPIO as GPIO
 import math
 
 coordinates=[]  #holds the waypoints to navigate to, odd indexs should be end of straight section of sidewalk.
@@ -21,7 +19,7 @@ object_detect=0 #thread the sonar
 class recognition:
 	def __init__(self):
 		#Begins camera connection
-		self.cap = cv2.VideoCapture(0)
+		self.cap = cv2.VideoCapture(1)
 	def get_img(self):
 		#update current image.
 		while 1:
@@ -32,7 +30,7 @@ class recognition:
 	def get_lines(self):
 		#function that returns a series of lines in the current image. Possibly using an input region of interest of the image.
 		pass
-	def wh_det(self,x1=0,x2=640,y1=470,y2=10,xit=10,yit=-10,slope_en=0,er_max=4,debug=0):    
+	def wh_det(self,x1=10,x2=630,y1=470,y2=10,xit=10,yit=-10,slope_en=0,er_max=4,debug=0):    
 		#create green pointers on sidewalk
 		count=[]
 		tot=0
@@ -60,58 +58,34 @@ class recognition:
 			    slopes.append(float(count[x1]-count[x1+1])*c1)   
 			return count,slopes
 		return count
-				
-                    
 
-class car:
-	def __init__(self):
-		#attribute of current speed.
-		# Right side motors. Pin definitions for H-bridge control
-		self.right_wheel = "P9_14"  # controls speed of right front motor via PWM
-		self.right_wheel_dir = "P9_12"   # combine M1 pins on upper and lower motor controllers into a single pin
-		# Left side motors. Pin definitions for H-bridge control
-		self.left_wheel = "P9_16"  # controls speed of left front motor via PWM
-		self.left_wheel_dir = "P8_11"  # combine M2 pins on upper and lower motor controllers into a single pin
-		# set pin directions as output
-		GPIO.setup(self.right_wheel, GPIO.OUT)
-		GPIO.setup(self.right_wheel_dir, GPIO.OUT)
-		GPIO.setup(self.left_wheel, GPIO.OUT)
-		GPIO.setup(self.left_wheel_dir, GPIO.OUT)
-
-		PWM.start(self.left_wheel, 0)
-		PWM.start(self.right_wheel, 0)
-	def forward(self):
-		GPIO.output(self.right_wheel_dir, GPIO.HIGH)
-		GPIO.output(self.left_wheel_dir, GPIO.HIGH)
-	def speed(self, duty=60, offset=0):
-		if offset>60:
-			x=60
-		elif offset<-60:
-			x=-60
+def follow_most_pixels1(gain,xit,yit):
+	l1=rec.wh_det(x1=0,x2=320,y1=200,y2=100,xit=xit, yit=yit,er_max=3,slope_en=0)
+	print l1
+	#first algorithim, follow most pixels
+	sum1=0
+	sum2=0
+	for x in range(0, len(l1)):
+		if x<len(l1)/2:
+			if l1[x]>10:
+				sum1+=1	
 		else:
-			x=offset
-		#changes the speed of individual motors to a specified inputs using PWM.
-		if x>0:
-			print 'turn right'
-			PWM.set_duty_cycle(self.left_wheel, duty)
-			PWM.set_duty_cycle(self.right_wheel, duty-x)
-		elif x<0:
-			print 'turn left'
-			PWM.set_duty_cycle(self.left_wheel, duty+x)
-			PWM.set_duty_cycle(self.right_wheel, duty)
-		else:
-			PWM.set_duty_cycle(self.left_wheel, duty)
-			PWM.set_duty_cycle(self.right_wheel, duty)
-	def speed1(self, duty=60, offset=0):
-		#changes the speed of individual motors to a specified inputs using PWM.
-		PWM.set_duty_cycle(self.left_wheel, duty+offset)
-		PWM.set_duty_cycle(self.right_wheel, duty-offset)	
-	def stop(self):
-		PWM.stop(self.right_wheel)
-		PWM.stop(self.left_wheel)
-		PWM.cleanup()
+			if l1[x]>10:
+				sum2+=1
+	offset=gain*(.5-(float(sum1+1)/(sum1+sum2+2)))#gain times error
+	#offset=gain*float(sum2-sum1)
+	if offset>15:
+		return 15
+	elif offset<-15:
+		return -15
+	else:
+		return offset
+	print 'left'+str(sum1)
+	print 'right'+str(sum2)
+	print 'offset'+str(offset)
+	return offset
 def follow_most_pixels(gain,xit,yit):
-	l1=rec.wh_det(x1=0,x2=320,y1=220,y2=60,xit=xit, yit=yit,er_max=3,slope_en=0)
+	l1=rec.wh_det(x1=0,x2=320,y1=200,y2=100,xit=xit, yit=yit,er_max=3,slope_en=0)
 	print l1
 	#first algorithim, follow most pixels
 	sum1=0
@@ -123,9 +97,19 @@ def follow_most_pixels(gain,xit,yit):
 			sum2+=l1[x]
 	offset=gain*(.5-(float(sum1+1)/(sum1+sum2+2)))#gain times error
 	#offset=gain*float(sum2-sum1)
+	#I control
+	if offset>15:
+		return 15
+	elif offset<-15:
+		return -15
+	else:
+		return offset
+	print 'left'+str(sum1)
+	print 'right'+str(sum2)
+	print 'offset'+str(offset)
 	return offset
 def control_distance(xit,yit,gain,side=1,dist=40):#side=1, right of sidewalk
-	l1,slopes=rec.wh_det(x1=0,x2=320,y1=235,y2=35,xit=xit, yit=yit,er_max=3,slope_en=1)
+	l1,slopes=rec.wh_det(x1=0,x2=320,y1=230,y2=40,xit=xit, yit=yit,er_max=5,slope_en=1)
 	print l1,slopes
 	if side==1:
 		#find sidewalk edge
@@ -145,8 +129,11 @@ def control_distance(xit,yit,gain,side=1,dist=40):#side=1, right of sidewalk
 			return 5#set low for straight sidewalks
 		slope=tot/it
 		if slope>0:
-			print 'condition 2, positive slope'
-			return 0
+			print 'condition 2'
+			return 5
+		elif slope>-1:
+			print 'condition 3'
+			return -10
 		#print str(zeroend*xit),l1[zeroend]*yit
 		b=(l1[zeroend]*yit)-slope*(zeroend-len(l1)/2)*xit
 		#intersection points
@@ -159,24 +146,26 @@ def control_distance(xit,yit,gain,side=1,dist=40):#side=1, right of sidewalk
 		#dist_to_sw=(-b)/slope
 		print 'distance to sidewalk, '+str(dist_to_sw)+' pixels'
 		offset=gain*(dist_to_sw-dist)
-		return offset
+		if offset>10:
+			return 10
+		elif offset<-10:
+			return -10
+		else:
+			return offset
 	else:
 		pass
 if __name__ == "__main__":
 	#create regocnition, GPS, and car instance.
 	rec=recognition()
-	car1=car()
 	
 	#Thread gps sonar and compass
-	time.sleep(3)
+	time.sleep(1)
 	while True:
 		if state=='drive':
 			if stage==0 or stage==1 or stage==2:
 				#follow compass while checking if path edges are too close.
 				it=0
 				start = time.time()
-				car1.forward()
-				I=0#form of integration
 				while True:
 					print '------iteration '+str(it)+' ---------'
 					#perform white detection
@@ -187,20 +176,15 @@ if __name__ == "__main__":
 					print xit,yit
 					#check sonar. if true change behavior and break
 					#check gps for change to turn state
-					P=follow_most_pixels(gain=60,xit=5,yit=-5)
-					offset=.9*I+P
-					I=I*.9+P#new I
-					print 'P ',P
-					#offset=control_distance(5,-5,gain=1,dist=40)
-					print 'offset ',offset
+					#if too close turn away from edge
 					
-					car1.speed(60, offset)
+					#offset=follow_most_pixels1(gain=60,xit=5,yit=-5)
+					offset=control_distance(5,-5,gain=1,dist=40)
+					print 'offset ',offset
 					it+=1
-					#if debug_mode==1:
-					#	cv2.imwrite('debug/debug_img'+str(it)+'.jpg', rec.img)
+					time.sleep(.8)
 					end=time.time()
-					if end-start>20:
-						car1.stop()
+					if end-start>30:
 						print 'stage '+str(stage)+' performed at '+str(it/(end-start))+' hertz'
 						exit()
 		elif state=='turn':
